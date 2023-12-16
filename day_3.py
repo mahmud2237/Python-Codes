108e2d7234bae14d9d7ef659370903c27c5a5299
mahmud2237 <smahmud2237@gmail.com>
2023-12-16 17:41:32 +0600

    Input works

---
+++ day_3.py
@@ -0,0 +1,33 @@
+number = input("Give me a number: ")
+group = input("Give me a collective noun for a group of things: ")
+thing = input("Give me the name of a weird or wacky thing: ")
+print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName, "thinks it is", whatYear)
+print("----Next code start here----")
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+name = input("what is your name?: ")
+print()
+print("There was a person called", name)
+print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
+print("----Next code start here----")
+
+food = input("Name a type of food: ")
+plant = input("Name a plant: ")
+cookingType = input("What is a way to cook something?")
+burntFood = input("How do you describe burnt food?")
+householdItem = input("Name something in your house: ")
+
+print()
+print("Tonight's dinner:")
+
+print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
