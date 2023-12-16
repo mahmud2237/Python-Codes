896f9a212a39fbad80d828eb1886aeba551df64d
mahmud2237 <smahmud2237@gmail.com>
2023-12-16 15:58:13 +0600

    added some more print value

---
+++ day_2.py
@@ -0,0 +1,45 @@
+"""
+Let's take a look at the input command and how that works. Input is when the user gives information to the computer. It's very similar to the print command, except that it'll show the message in the console then wait until the user has typed something into the console and pressed enter. Let's try it!
+input asks for something, takes it, but then has nowhere to put it. We can change that with a variable which is a value that we can use to name and store data. (Remember, David's box for David's phone?)
+Naming variables like:
+***You can give a variable any name you want, but you can't use spaces. You can use:
+1. underscores_between_words
+2. camelCaseToMakeItEasierToRead
+"""
+myName = input("What is your Nickname?: ")
+myAge = input("How old are you?: ")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit?: ")
+print("OF COURSE YOU DO!")
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Replit is")
+print(replit)
+myGrandma = input("How's your Grandma doing?😘: ")
+print(myGrandma)
+myLunch = input("What are you having for lunch?: ")
+print("Hmm, it sounds like you really should just order")
+print(myLunch)
+print("as soon as possible!")
+print()
+print()
+print("Definitely not a Phishing Scam")
+print()
+myName = input("Your Name: ")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print("cardNumber")
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+
