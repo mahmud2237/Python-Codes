24c0ffc82ad3c1e65d1a86089426aa71e97d8394
mahmud2237 <smahmud2237@gmail.com>
2023-12-18 17:19:10 +0600

    If...Elif...Else statement

---
+++ day_6.py
@@ -0,0 +1,25 @@
+print("SECURE LOGIN HERE")
+username = input("What's your username?: ")
+password = input("What's your password?: ")
+if username == "mahmud" and password == "pass":
+  print("Welcome Mahmud!")
+elif username == "mim" and password == "mimi":
+  print("Welcome Mim!")
+else:
+  print("Go Away!")
+
+
+
+"""
+season = input("what is your favorite season?: ")
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+  print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
+"""
