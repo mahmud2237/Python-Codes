35bc88476fb5e9d1586f943c4ca8faf21e3ac675
mahmud2237 <smahmud2237@gmail.com>
2023-12-18 16:46:06 +0600

    If...Else statement works

---
+++ day_5.py
@@ -0,0 +1,19 @@
+print("Which character are you from 'Avengers?'")
+print()
+print("Answer these questions and let's find out.")
+print()
+print("Please use Y or N for yes and no.")
+like_green = input("Do you like the color green?: ")
+if like_green == "Y":
+    print("You must be the Hulk!")
+like_iron = input("Do you think building robots is cool?: ")
+if like_iron == "Y":
+    print("You must be Iron Man. Cool suit!")
+timeTravel = input("Do you like traveling trough time?: ")
+if timeTravel == "Y":
+    print("You must be Captain America!")
+strong = input("Are you super strong?: ")
+if strong == "Y":
+    print("You have got to be Thor!")
+else:
+    print("I guess you are not like anyone from 'Avengers.'")
