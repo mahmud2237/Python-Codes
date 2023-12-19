c0dcad98c9e164c1facffd67af36b3193da691e2
Md. Shahed Mahmud <97587123+mahmud2237@users.noreply.github.com>
2023-12-19 21:09:50 +0600

    Create day_8.py
    Affirmations

---
+++ day_8.py
@@ -0,0 +1,25 @@
+name = input("What is your name? ")
+print("Hello ", name,"!")
+#ask current day of the weak
+day = input("What day of the weak is it? ")
+#ask favorite things
+fav1 = input("What is your favorite thing? ")
+fav2 = input("What is your second favorite thing? ")
+fav3 = input("What is your third favorite thing? ")
+print("Your favorite things are", fav1, fav2, fav3)
+if name == "Dave" or name == "dave":
+  print("Goodjob Dave")
+elif name == "David" or name== "david":
+ DOW = input("What is the day of the week?")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a great Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("You look great in that color", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("You look chipper today", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"you are doing a great job!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "it's FRIDAY!")
+else:
+ print("I do not know your name, but I hope you are having a great day!")
