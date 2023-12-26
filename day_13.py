aa1de98ffae486d380bb8dfd43e503ff353e3a52
mahmud2237 <smahmud2237@gmail.com>
2023-12-26 06:49:48 +0600

    Grade Calculator Project

--- day_13.py
+++ day_13.py
@@ -23,7 +23,7 @@ else:
     # Output an error message for an invalid mark
     print("Error: Invalid exam mark. Please enter a mark between 0 and 100.")
 """
-
+"""
 #Write a program that will input a exam mark from the keyboard.
 exam_mark = int(input("Enter your exam mark: "))
 #The program needs to find out the grade for that mark and then output that grade to the screen.
@@ -46,4 +46,32 @@ elif exam_mark >=0 and exam_mark<=49:
 #The program should output an error message if the mark entered is not in the correct.
 else:
   print("Error: Invalid Exam mark. Please enter a mark between 0 and 100.")
-  
+"""
+print("Exam Grade Calculator")
+print()
+name_of_exam = input("Name of exam: ")
+print()
+total_score = int(input("Max. Possible Score:"))
+your_score = int(input("Your score: "))
+print()
+
+number_score = float(your_score / total_score)
+final_number = round(number_score, 2)
+final_perc = round(float(your_score / total_score)*100, 2)
+
+print("You got",final_perc,"%")
+
+if final_number >= .90:
+  print("Your letter score is an A+")
+elif final_number >= .80 and final_number <= .89:
+  print("Your letter grade is an A-.")
+elif final_number >= .70 and final_number <= .79:
+  print("Your letter score is a B.")
+elif final_number >= .60 and final_number <= .69:
+  print("Your letter grade is a C.")
+elif final_number >= .50 and final_number <= .59:
+  print("Your letter grade is a D.")
+elif final_number <= .49:
+  print("Your letter grade is a U.")
+else: 
+  print("Error! Try again! Enter a valid mark between 0 and 100.")
